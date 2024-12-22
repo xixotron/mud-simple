73c8519898fda755453adaffd1bfc1e445f5abcd
xixotron <xixotron89@gmail.com>
2024-12-22 21:24:05 +0000

    gitignore rooms.py

---
+++ rooms.py
@@ -0,0 +1,10 @@
+rooms = {
+    "Tavern": {
+        "description": "You're in a tavern warmed by an open fire.",
+        "exits": {"outside": "Outside"},
+    },
+    "Outside": {
+        "description": "You're standing outside a tavern. It's raining.",
+        "exits": {"inside": "Tavern"},
+    }
+}
