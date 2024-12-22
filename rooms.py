7d724d63ac0289b122868b1d40c5d398534b8f96
KikoVF <kikovf@gmail.com>
2024-12-23 00:11:10 +0100

    Update rooms.py

--- rooms.py
+++ rooms.py
@@ -2,11 +2,11 @@
 
 rooms = {
     "casa": {
-        "description": "Estás en una sucia casa.",
-        "exits": {"outside": "fuera"},
+        "descripcion": "Estas en una sucia casa.",
+        "salidas": {"fuera"},
     },
     "fuera": {
-        "description": "Estás fuera de una casa. Está lloviendo.",
-        "exits": {"inside": "casa"},
+        "descripcion": "Estás fuera de una casa. Está lloviendo.",
+        "salidas": {"inside": "casa"},
     }
 }
